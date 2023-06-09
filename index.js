106a97db83cdabe0f87300b86f55c922b63350a3
ariful-islam <arifulislam204909@gmail.com>
2023-06-09 12:47:40 +0600

    create new user on the database

--- index.js
+++ index.js
@@ -24,14 +24,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+
+    const classCollection =client.db("languageDB").collection("classes");
+    const usersCollection =client.db("languageDB").collection("users");
+
+    app.get("/classes", async (req, res)=>{
+        const result =await classCollection.find().toArray();
+        res.send(result);
+    });
+
+    // create new user
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exists' })
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+  
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
