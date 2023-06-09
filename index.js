225c0b5b0c8c765ac1ccd1d7f3f8bc523a39de2e
ariful-islam <arifulislam204909@gmail.com>
2023-06-10 01:09:02 +0600

    get selected class by email api added

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const port =process.env.PORT || 5000;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 //middle ware
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bttisu.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,62 +17,68 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const classCollection = client.db("languageDB").collection("classes");
+    const usersCollection = client.db("languageDB").collection("users");
+    const cartCollection = client.db("languageDB").collection("carts");
 
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
 
-    const classCollection =client.db("languageDB").collection("classes");
-    const usersCollection =client.db("languageDB").collection("users");
+    //selected class added
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
 
-    app.get("/classes", async (req, res)=>{
-        const result =await classCollection.find().toArray();
-        res.send(result);
+    //get selected classes by email
+    app.get("/carts/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+      console.log(result);
     });
 
     // create new user
 
-    app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-  
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
 
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
 
-
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
-   // await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-app.get("/", (req,res)=>{
-    res.send("Language server is running")
+app.get("/", (req, res) => {
+  res.send("Language server is running");
 });
 
-app.listen(port, ()=>{
-    console.log(`language server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`language server running on port ${port}`);
+});
